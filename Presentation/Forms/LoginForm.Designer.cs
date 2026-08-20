4cb95979634f000a337bf8d90d3ef893e361e8f5
Ammar2080 <3.19203721e+08+Ammar2080@users.noreply.github.com>
2026-08-20 22:07:55 +0000

    fix: Clean up solution file (.sln), add explicit LoginForm and MainForm Designer.cs files, and fix project dependencies for VS 2019

---
+++ Presentation/Forms/LoginForm.Designer.cs
@@ -0,0 +1,30 @@
+namespace WarehouseManagement.Presentation.Forms
+{
+    partial class LoginForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Name = "LoginForm";
+            this.Text = "Smart Warehouse - Login";
+            this.ResumeLayout(false);
+        }
+    }
+}
